e122ba905d3ad793db04999340d88b4385f3c903
keeshonmoore <servvvone@gmail.com>
2025-08-16 18:48:45 -0700

    fixed 'vite.config.js'

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
-   import path from 'path'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-   // https://vitejs.dev/config/
-   export default defineConfig({
-     base: 'https://app.buildhire.io/',
-     plugins: [react()],
-     resolve: {
-       alias: {
-         '@': path.resolve(__dirname, './src'),
-       },
-     },
-   })
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
